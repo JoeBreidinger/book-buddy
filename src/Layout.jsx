42333080c6c58c54c0be2b01cffb8f02fe1dbb0d
Joseph Breidinger <joseph.breidinger@gmail.com>
2025-10-19 16:53:46 -0400

    added Navbar component to main page

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router";
+import Navbar from "./Navbar";
 
 export default function Layout() {
   return (
     <>
-      <header>Navbar</header>
+      <Navbar />
       <main>
         <Outlet />
       </main>
