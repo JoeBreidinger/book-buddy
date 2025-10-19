8c5c062de803794841d0f53dfbb4f6ff3fa2e9cc
Joseph Breidinger <joseph.breidinger@gmail.com>
2025-10-19 17:42:40 -0400

    added login and register components and connected to API to create accounts and login

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router";
 import Layout from "./Layout";
 import Books from "./Books";
 import BookDetails from "./BookDetails";
+import Login from "./auth/Login";
+import Register from "./auth/Register";
 
 // Returning route components to allow for easy navigation through pages on UI
 export default function App() {
@@ -11,6 +13,8 @@ export default function App() {
         <Route path="/" element={<Books />} />
         <Route path="/books" element={<Books />} />
         <Route path="/books/:id" element={<BookDetails />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Route>
     </Routes>
   );
